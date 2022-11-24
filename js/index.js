da3222394971e7d83c71de23d42c12f10b36f108
pol6pil <pol6pil@gmail.com>
2022-11-24 19:27:15 +0100

    Calculadora Objetos

--- js/index.js
+++ js/index.js
@@ -1,44 +1,102 @@
 'use strict';
 
+//Mostramos el mensaje de bienvenida al usuario
 alert('Bienvenido');
+
+//Creamos un objeto calculadora que tiene una propiedad lastResult que por defecto tendra de valor 0 y 4 metodos para realizar las operaciones
+let Calculadora = {
+    lastResult: 0,
+
+    sumar(operando1, operando2) {
+        //Almacenamos el resultado en la propiedad lastResult
+        this.lastResult = operando1 + operando2;
+        return operando1 + operando2;
+    },
+
+    restar(operando1, operando2) {
+        //Almacenamos el resultado en la propiedad lastResult
+        this.lastResult = operando1 - operando2;
+        return operando1 - operando2;
+    },
+
+    multiplicar(operando1, operando2) {
+        //Almacenamos el resultado en la propiedad lastResult
+        this.lastResult = operando1 * operando2;
+        return operando1 * operando2;
+    },
+
+    dividir(operando1, operando2) {
+        //Si el segundo operador es 0 mostramos que no se puede dividir y como ultimo resultado guardamos 0
+        if (operando2 !== 0) {
+            this.lastResult = operando1 / operando2;
+            return operando1 / operando2;
+        }
+        else {
+            this.lastResult = 0;
+            return 'No se puede dividir entre 0';
+        }
+    },
+
+}
 
 do {
 
 
     let operador = pedirOperador();
+    if (operador === null) {
+        break;
+    }
     let operandos;
     let operando1;
-    let operando2;  
+    let operando2;
 
 
-    let sacarOperando1 = function(operandos) {
+    let sacarOperando1 = function (operandos) {
         let operando1 = operandos.substring(0, operandos.indexOf(' '));
-            operando1 = operando1.trim();
-            operando1 = Number(operando1);
-            return operando1;
-    }    
+        operando1 = operando1.trim();
 
-    let sacarOperando2 = function(operandos) {
+        if (operando1 === 'R') {
+            operando1 = Calculadora.lastResult;
+        }
+        else {
+            operando1 = Number(operando1);
+        }
+
+        return operando1;
+    }
+
+    let sacarOperando2 = function (operandos) {
         let operando2 = operandos.substring(operandos.indexOf(' '), operandos.length);
         operando2 = operando2.trim();
-        operando2 = Number(operando2);
-            return operando2;
-    }  
 
-    let pedirOperandos = function() {
-            operandos = prompt('Inserte los dos operandos separados por un espacio');
-            if (!Boolean(operandos)) {
-                operandos = 'invalido';
-            }
-            operandos = operandos.trim();
+        if (operando2 === 'R') {
+            operando2 = Calculadora.lastResult;
+        }
+        else {
+            operando2 = Number(operando2);
+        }
 
-            return operandos;
+        return operando2;
+    }
+
+    let pedirOperandos = function () {
+        operandos = prompt('Inserte los dos operandos separados por un espacio');
+        if (operandos === null) {
+            return null;
+        }
+
+        if (!Boolean(operandos)) {
+            operandos = 'invalido';
+        }
+        operandos = operandos.trim();
+
+        return operandos;
     }
 
     do {
-       
+
         operandos = pedirOperandos();
-        
+
         operando1 = sacarOperando1(operandos);
 
         operando2 = sacarOperando2(operandos);
@@ -47,29 +105,20 @@ do {
             alert('Operandos invalidos')
         }
 
-    } while (!isFinite(operando1) || !isFinite(operando2));  
+    } while (!isFinite(operando1) || !isFinite(operando2));
 
-    let sumar = (operando1, operando2) => operando1 + operando2;
-
-    let restar = (operando1, operando2) => operando1 - operando2;
-
-    let multiplicar = (operando1, operando2) => operando1 * operando2;
-
-    let dividir = (operando2 !== 0) ?
-    () => operando1 / operando2:
-    () => 'No se puede dividir entre 0';
 
     switch (operador) {
-        case '+': alert(sumar(operando1, operando2));
+        case '+': alert(Calculadora.sumar(operando1, operando2));
             break;
 
-        case '-': alert(restar(operando1, operando2));
+        case '-': alert(Calculadora.restar(operando1, operando2));
             break;
 
-        case '*': alert(multiplicar(operando1, operando2));
+        case '*': alert(Calculadora.multiplicar(operando1, operando2));
             break;
 
-        case '/': alert(dividir(operando1, operando2));
+        case '/': alert(Calculadora.dividir(operando1, operando2));
             break;
     }
 
@@ -79,6 +128,10 @@ function pedirOperador() {
     let operador;
     do {
         operador = prompt('Inserte el operador deseado (+, -, * o /)');
+        if (operador === null) {
+            break;
+        }
+
         if (!Boolean(operador)) {
             operador = 'invalido';
         }
